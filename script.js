976a4fad9126ebc3d7a68bad9b52cc71efc1ca6d
zasanm <zasanm@bbcag.ch>
2022-06-28 13:17:00 +0200

    Questions

--- script.js
+++ script.js
@@ -47,3 +47,66 @@ function setNextQuestion() {
     }
   }
   
+  function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+      setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+      nextButton.classList.remove('hide')
+    } else {
+      startButton.innerText = 'Restart'
+      startButton.classList.remove('hide')
+    }
+  }
+  
+  function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+      element.classList.add('correct')
+    } else {
+      element.classList.add('wrong')
+    }
+  }
+  
+  function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+  }
+  
+  const questions = [
+    {
+      question: 'How many stripes are on the US flag?',
+      answers: [
+        { text: '13', correct: true },
+        { text: '14', correct: false }
+      ]
+    },
+    {
+      question: 'How many time zones are there in Russia?',
+      answers: [
+        { text: '0', correct: false },
+        { text: '8', correct: false },
+        { text: '11', correct: true },
+        { text: '9', correct: false }
+      ]
+    },
+    {
+      question: 'How long does it take to get to the Mariana Trench?',
+      answers: [
+        { text: '4h', correct: true },
+        { text: '1 Month', correct: false },
+        { text: '6Month', correct: false },
+        { text: 'IDK', correct: false }
+      ]
+    },
+    {
+      question: 'how fast is the fastest animal',
+      answers: [
+        { text: '157,03', correct: false },
+        { text: '103', correct: true }
+      ]
+    }
+  ]
