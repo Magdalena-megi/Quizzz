69d70c5c4da51a0a083feeec49b0837088e88231
zasanm <zasanm@bbcag.ch>
2022-06-29 10:33:52 +0200

    zu

--- script.js
+++ script.js
@@ -78,91 +78,94 @@ function clearStatusClass(element) {
 }
 
 const questions = [
-    {
-        question: 'How many stripes are on the US flag?',
-      answers: [
-        { text: '13', correct: true },
-        { text: '14', correct: false }
-      ]
-    },
-    {
-     
-      question: 'How many time zones are there in Russia?',
-      answers: [
-        { text: '0', correct: false },
-        { text: '8', correct: false },
-        { text: '11', correct: true },
-        { text: '9', correct: false }
-      ]
-    },
-    {
-      question: 'How long does it take to get to the Mariana Trench?',
-      answers: [
-        { text: '4h', correct: true },
-        { text: '1 Month', correct: false },
-        { text: '6Month', correct: false },
-        { text: 'IDK', correct: false }
-      ]
-    },
-    {
-        question: 'How many letters does the Hawaiian alphabet have?',
-        answers: [
-          { text: '24', correct: false },
-          { text: '19', correct: false },
-          { text: '54', correct: false },
-          { text: '12', correct: true }
-        ]
-      },
-      {
-        question: 'Which country is the second largest in the world by area?',
-        answers: [
-          { text: 'Russia', correct: false },
-          { text: 'Canada', correct: true },
-          { text: 'Srbiaaaa', correct: false },
-          { text: 'Africa', correct: false }
-        ]
-      },
-      {
-        question: 'since when does the bbw exist?',
-        answers: [
-          { text: '1836', correct: true },
-          { text: '1854', correct: false },
-          { text: '1976', correct: false },
-          { text: '1983', correct: false }
-        ]
-      },   
-       {
-        question: 'When did Christopher Columbus discover America?',
-        answers: [
-          { text: '1476', correct: false },
-          { text: '2022', correct: false },
-          { text: '1598', correct: false },
-          { text: '1492 ', correct: true }
-        ]
-      }, {
-        question: 'which is the best selling book in the world?',
-        answers: [
-          { text: 'Harry Potter', correct: false },
-          { text: 'Bible', correct: true },
-          { text: 'Koran', correct: false },
-          { text: 'Don Quijote ', correct: false }
-        ]
-      },
-      {
-        question: 'which is the best selling book in the world?',
-        answers: [
-          { text: 'Harry Potter', correct: false },
-          { text: 'Bible', correct: true },
-          { text: 'Koran', correct: false },
-          { text: 'Don Quijote ', correct: false }
-        ]
-      },
-    {
-      question: 'How fast is the fastest animal?',
-      answers: [
-        { text: '157,03', correct: false },
-        { text: '103', correct: true }
-      ]
-    }
-    
-  ]
+  {
+    question: 'How many stripes are on the US flag?',
+    answers: [
+      { text: '13', correct: true },
+      { text: '14', correct: false }
+    ]
+  },
+  {
+
+    question: 'How many time zones are there in Russia?',
+    answers: [
+      { text: '0', correct: false },
+      { text: '8', correct: false },
+      { text: '11', correct: true },
+      { text: '9', correct: false }
+    ]
+  },
+  {
+    question: 'How long does it take to get to the Mariana Trench?',
+    answers: [
+
+      { text: '4h', correct: true },
+      { text: '1 Month', correct: false },
+      { text: '6Month', correct: false },
+      { text: 'IDK', correct: false }
+    ]
+  },
+
+  {
+
+    question: 'How many letters does the Hawaiian alphabet have?',
+    answers: [
+      { text: '24', correct: false },
+      { text: '19', correct: false },
+      { text: '54', correct: false },
+      { text: '12', correct: true }
+    ]
+  },
+  {
+    question: 'Which country is the second largest in the world by area?',
+    answers: [
+      { text: 'Russia', correct: false },
+      { text: 'Canada', correct: true },
+      { text: 'Srbiaaaa', correct: false },
+      { text: 'Africa', correct: false }
+    ]
+  },
+  {
+    question: 'since when does the bbw exist?',
+    answers: [
+      { text: '1836', correct: true },
+      { text: '1854', correct: false },
+      { text: '1976', correct: false },
+      { text: '1983', correct: false }
+    ]
+  },
+  {
+    question: 'When did Christopher Columbus discover America?',
+    answers: [
+      { text: '1476', correct: false },
+      { text: '2022', correct: false },
+      { text: '1598', correct: false },
+      { text: '1492 ', correct: true }
+    ]
+  }, {
+    question: 'which is the best selling book in the world?',
+    answers: [
+      { text: 'Harry Potter', correct: false },
+      { text: 'Bible', correct: true },
+      { text: 'Koran', correct: false },
+      { text: 'Don Quijote ', correct: false }
+    ]
+  },
+  {
+    question: 'which is the best selling book in the world?',
+    answers: [
+      { text: 'Harry Potter', correct: false },
+      { text: 'Bible', correct: true },
+      { text: 'Koran', correct: false },
+      { text: 'Don Quijote ', correct: false }
+    ]
+  },
+  {
+    question: 'How fast is the fastest animal?',
+    answers: [
+      { text: '157,03', correct: false },
+      { text: '103', correct: true }
+    ]
+  }
+
+]
